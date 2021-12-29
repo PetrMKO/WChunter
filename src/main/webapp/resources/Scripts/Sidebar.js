b57210ac4d96895ce7c30c61f6185a8186b4e8c1
Petr <pmarfutenko3010@gmail.com>
2021-12-30 04:39:19 +0500

    .kj

--- src/main/webapp/resources/Scripts/Sidebar.js
+++ src/main/webapp/resources/Scripts/Sidebar.js
@@ -182,7 +182,7 @@ export function addPoints(map, geoCollection){
             console.log(points);
             // points.sort((a, b) => b.mark - a.mark || getLenthToUser(geolat, geolong, +b.latitude, +b.longitude) - getLenthToUser(geolat, geolong, +a.latitude, +a.longitude));
             coordsArr = getcoords(points);
-            console.log(cooordsArr);
+            console.log(coordsArr);
             points.forEach((point) => {
                 console.log(point);
                 let imageURL = '/resources/images/ToiletIcon.png';
